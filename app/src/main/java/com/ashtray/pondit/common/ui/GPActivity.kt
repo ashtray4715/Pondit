263966364d1af2e0ef779c278cae6f3ca9317d36
Ashtray4715 <gobinda.paul.4715@gmail.com>
2022-08-23 20:09:23 +0600

    Adding GPActivity.kt

--- app/src/main/java/com/ashtray/pondit/common/ui/GPActivity.kt
+++ app/src/main/java/com/ashtray/pondit/common/ui/GPActivity.kt
@@ -1,11 +1,12 @@
-package com.ashtray.pondit
+package com.ashtray.pondit.common.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ashtray.pondit.R
 
-class MainActivity : AppCompatActivity() {
+class GPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.gp_activity)
     }
 }
